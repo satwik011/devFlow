09e928da3eed13a6eec4b8e5a4540d89029c2019
Satwik <satwik.nakhate@nova-tekno.com>
2025-10-30 18:43:55 +0530

    feat: add assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="">
-      <h1>Welcome to Next.js !</h1>
-    </div>
+    <>
+      <div className="font-inter">
+        <h1>Welcome to Next.js !</h1>
+      </div>
+      <div className="font-space-grotesk">
+        <h1>Welcome to Next.js !</h1>
+      </div>
+    </>
   );
 }

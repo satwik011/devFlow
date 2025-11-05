4abd96187eae95425c26c6237651066c0d899112
Satwik <satwik.nakhate@nova-tekno.com>
2025-11-05 21:13:28 +0530

    feat: add auth form component

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+import AuthForm from "@/components/forms/AuthForms";
+import { signInSchema } from "@/lib/validations";
+
 const Page = () => {
-  return <div>Sign in</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      defaultValues={{ email: "", username: "" }}
+      schema={signInSchema}
+      onSubmit={(data) =>
+        Promise.resolve({
+          success: true,
+          data,
+        })
+      }
+    />
+  );
 };
 
 export default Page;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+import AuthForm from "@/components/forms/AuthForms";
+import { SignUpSchema } from "@/lib/validations";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      defaultValues={{ email: "", username: "", name: "", password: "" }}
+      schema={SignUpSchema}
+      onSubmit={(data) =>
+        Promise.resolve({
+          success: true,
+          data,
+        })
+      }
+    />
+  );
 };
 
 export default Page;

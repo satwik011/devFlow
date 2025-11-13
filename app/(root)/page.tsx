fa225fe7c832fd9de5c5f38841ea60aa4ab67f1d
Satwik <satwik.nakhate@nova-tekno.com>
2025-11-13 19:33:19 +0530

    feat: start design for add question page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,28 @@
-import { auth, signOut } from "@/auth";
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
   return (
-    <div className="font-inter">
-      <h1>Welcome to Next.js !</h1>
-      {/* <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({
-            redirectTo: ROUTES.SIGN_IN,
-          });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form> */}
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button className="primary-gradient !text-light-900_ min-h-[46px] px-4 py-3" asChild>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+
+        <p>Question Card 1</p>
+
+        <p>Question Card 1</p>
+      </div>
+    </>
   );
 };
 

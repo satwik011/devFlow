b138f01336318c3eabb3dbe2ab3e5598501ab5f3
Satwik <satwik.nakhate@nova-tekno.com>
2026-07-28 12:41:55 +0530

    implement error handlers

---
+++ lib/http-errors.ts
@@ -0,0 +1,55 @@
+export class RequestError extends Error {
+  // base of request error
+  statusCode: number;
+  errors?: Record<string, string[]>;
+  // what are we accepting into this class for
+  constructor(statusCode: number, message: string, errors?: Record<string, string[]>) {
+    //call the parent class and passes the message
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.name = "RequestError";
+  }
+}
+
+export class ValidationError extends RequestError {
+  constructor(fieldErrors: Record<string, string[]>) {
+    const message = ValidationError.formatFieldErrors(fieldErrors);
+    super(400, message, fieldErrors);
+    this.name = "ValidationError";
+    this.errors = fieldErrors;
+  }
+  static formatFieldErrors(errors: Record<string, string[]>): string {
+    const formattedMessages = Object.entries(errors).map(([field, messages]) => {
+      const fieldName = field.charAt(0).toUpperCase() + field.slice(1);
+
+      if (messages[0] === "Required") {
+        return `${fieldName} is required`;
+      } else {
+        return messages.join(" and ");
+      }
+    });
+    return formattedMessages.join(", ");
+  }
+}
+
+export class NotFoundError extends RequestError {
+  constructor(resource: string) {
+    super(404, `${resource} not found`);
+    this.name = "NotFoundError";
+  }
+}
+
+export class ForbiddenError extends RequestError {
+  constructor(message: string = "Forbidden") {
+    super(403, message);
+    this.name = "ForbiddenError";
+  }
+}
+
+export class UnauthorizedError extends RequestError {
+  constructor(message: string = "Unauthorized") {
+    super(401, message);
+    this.name = "UnauthorizedError";
+  }
+}

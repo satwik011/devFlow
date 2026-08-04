93c8b3a243096ba94410cb478154d5032f74482a
Satwik <satwik.nakhate@nova-tekno.com>
2026-08-04 16:12:39 +0530

    implement a fetch handler

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import { auth } from "@/auth";
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import { api } from "@/lib/handlers/api";
+import handleError from "@/lib/handlers/error";
 import Link from "next/link";
 
 const questions = [
@@ -46,10 +47,19 @@ const questions = [
     createdAt: new Date("2021-09-01"),
   },
 ];
+const test = async () => {
+  try {
+    return await api.users.getAll();
+  } catch (error) {
+    return handleError(error);
+  }
+};
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 const Home = async ({ searchParams }: SearchParams) => {
+  const users = await test();
+  console.log(users);
   const { query = "", filter = "" } = await searchParams;
   const filteredQuestions = questions.filter((question) => {
     const matchesQuery = question.title.toLowerCase().includes(query.toLowerCase());
--- app/api/users/email/route.ts
+++ app/api/users/email/route.ts
@@ -1,6 +1,7 @@
 import User from "@/database/user.model";
 import handleError from "@/lib/handlers/error";
 import { NotFoundError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
 import { UserSchema } from "@/lib/validations";
 import { APIErrorResponse } from "@/types/global";
 import { NextResponse } from "next/server";
@@ -8,6 +9,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const { email } = await request.json();
   try {
+    await dbConnect();
     const validatedData = UserSchema.partial().safeParse({ email });
     if (!validatedData) throw new NotFoundError("User");
 

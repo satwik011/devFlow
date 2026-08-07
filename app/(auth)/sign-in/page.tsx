55b64da76b461adf2516de53e21bf0ff9ebeea61
Satwik <satwik.nakhate@nova-tekno.com>
2026-08-07 23:31:30 +0530

    implement signin Auth

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,19 +1,15 @@
 "use client";
 import AuthForm from "@/components/forms/AuthForms";
+import { signInWithCredentials } from "@/lib/actions/auth.actions";
 import { signInSchema } from "@/lib/validations";
 
 const Page = () => {
   return (
     <AuthForm
       formType="SIGN_IN"
-      defaultValues={{ email: "", username: "" }}
+      defaultValues={{ email: "", password: "" }}
       schema={signInSchema}
-      onSubmit={(data) =>
-        Promise.resolve({
-          success: true,
-          data,
-        })
-      }
+      onSubmit={signInWithCredentials}
     />
   );
 };
